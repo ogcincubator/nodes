b664857eac33723fec400f75ac2203327d39d90b
doublebyte1 <info@doublebyte.net>
2025-07-09 16:10:25 +0100

    - Added support to enabling/disabling nodes, according to their validTo date.

--- src/AppVector.js
+++ src/AppVector.js
@@ -13,6 +13,22 @@ L.Icon.Default.mergeOptions({
   shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
+const defaultIcon = new L.Icon.Default();
+
+const greyIcon = new L.Icon({
+  iconUrl: require('./marker-icon-grey.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+function isCurrent(datetime){
+  const now = new Date();
+  return datetime > now.toISOString();
+  }
+
 function AppVector() {
   return (
     <div className="App">
@@ -29,19 +45,26 @@ function AppVector() {
           url="https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}{r}.png"
           maxZoom={20}
         />
-        <MarkerClusterGroup>
-          {nodes.features.map((node, index) => (
-            <Marker
-              key={node.id}
-              position={[node.geometry.coordinates[1], node.geometry.coordinates[0]]}
-            >
-              <Popup>
-                {node.properties.name.name}
-                <br/>
-                <a href={node.properties.hasMetadata.href} target="_blank" rel="noopener noreferrer">More info</a>
-              </Popup>
-            </Marker>
-            ))}
+      <MarkerClusterGroup>
+      {nodes.features.map((node, index) => {
+        const isEnabled = isCurrent(node.properties.validTo);
+        const icon = isEnabled ? defaultIcon : greyIcon;
+
+        return (
+          <Marker
+            key={node.id}
+            position={[node.geometry.coordinates[1], node.geometry.coordinates[0]]}
+            icon={icon}
+          >
+            <Popup>
+              {node.properties.name.name}
+              <br/>
+              <a href={node.properties.hasMetadata.href} target="_blank" rel="noopener noreferrer">More info</a>
+            </Popup>
+          </Marker>
+        );
+      })}
+
         </MarkerClusterGroup>
       </MapContainer>
     </div>
